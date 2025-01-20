79e781b8f6dec5c55b2f20a76c87d0d0094d3942
lordypopdy-lab <deelordpopdy2@gmail.com>
2025-01-20 08:43:53 -0800

    safe

--- tradevisterclient/src/App.jsx
+++ tradevisterclient/src/App.jsx
@@ -14,7 +14,7 @@ import { Toaster } from 'react-hot-toast';
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-axios.defaults.baseURL = 'http://localhost:8080';
+axios.defaults.baseURL = 'https://tradevisterserver.vercel.app';
 axios.defaults.withCredentials = true;
 
 //https://tradevisterserver.vercel.app
