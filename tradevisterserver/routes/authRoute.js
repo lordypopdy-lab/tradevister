bfdabc504d74c036c8db8c13c9c80ae510ba3d98
James Issac <jamesissac@Jamess-MacBook-Pro.local>
2026-02-19 15:50:29 -0800

    safe

--- tradevisterserver/routes/authRoute.js
+++ tradevisterserver/routes/authRoute.js
@@ -6,6 +6,7 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
   "https://tradevister.vercel.app",
+  "https://tradevister-kyc.vercel.app",
 ];
 
 const corsOptions = {
